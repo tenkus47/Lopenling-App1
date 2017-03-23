a733d2f159b1855bf27ec1d1f45968ae87de385a
Tim Lewis <tim@phurba.net>
2017-03-23 21:35:23 +1300

    Add support for the add and remove annotation API.

--- parkhang/frontend/app/api/index.js
+++ parkhang/frontend/app/api/index.js
@@ -28,6 +28,48 @@ export function fetchTextWitnesses(text) {
 }
 
 export function fetchWitnessAnnotations(witness) {
-    const url = '/api/texts/'+String(witness.text)+'/witnesses/'+witness.id+'/annotations/';
+    const url = '/api/texts/'+witness.text+'/witnesses/'+witness.id+'/annotations/';
     return getData(url);
+}
+
+function getApplyAnnotationUrl(witness) {
+    console.log('getApplyAnnotationUrl: %o', witness);
+    return '/api/texts/'+witness.text.id+'/witnesses/'+witness.id+'/applied_annotations/';
+}
+
+export function fetchAppliedUserAnnotations(witness) {
+    const url = getApplyAnnotationUrl(witness);
+    return getData(url);
+}
+
+export function applyAnnotation(annotation) {
+    const url = getApplyAnnotationUrl(annotation.witness);
+
+    return new Promise((resolve, reject) => {
+        axios.post(url, {
+                annotation_id: annotation.id
+            })
+            .then(function(response){
+                resolve(response.data);
+            })
+            .catch(function (error) {
+                reject(error);
+            })
+    });
+}
+
+export function removeAnnotation(annotation) {
+    const url = getApplyAnnotationUrl(annotation.witness);
+
+    return new Promise((resolve, reject) => {
+        axios.delete(url, {
+                annotation_id: annotation.id
+            })
+            .then(function(response){
+                resolve(response.data);
+            })
+            .catch(function (error) {
+                reject(error);
+            })
+    });
 }
