d42c23166b58ad498f3beae7cd379fd0f232965c
Tim Lewis <tim@phurba.net>
2017-04-30 22:24:37 +1200

    Refactored API to reduce repeated code.

--- parkhang/frontend/app/api/index.js
+++ parkhang/frontend/app/api/index.js
@@ -3,76 +3,89 @@ import axios from 'axios'
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-function getData(url) {
+const GET = 'get';
+const POST = 'post';
+const PUT = 'put';
+const DELETE = 'delete';
+
+function request(method, url, data=null) {
+    let req = null;
+    switch(method) {
+        case POST:
+            req = axios.post;
+            break;
+        case PUT:
+            req = axios.put;
+            break;
+        case DELETE:
+            req = axios.delete;
+            break;
+        case GET:
+        default:
+            req = axios.get;
+            break;
+    }
+
     return new Promise((resolve, reject) => {
-        axios.get(url)
-            .then(function(response){
+        req(url, data)
+            .then((response) => {
                 resolve(response.data);
             })
-            .catch(function (error) {
+            .catch((error) => {
+                console.dir(error);
                 reject(error);
             })
     });
 }
 
+// GET DATA
+
 export function fetchSources() {
     const url = '/api/sources/';
-    return getData(url);
+    return request(GET, url);
 }
 
 export function fetchTexts() {
     const url = '/api/texts/';
-    return getData(url);
+    return request(GET, url);
 }
 
 export function fetchTextWitnesses(text) {
     const url = '/api/texts/'+String(text.id)+'/witnesses/'
-    return getData(url);
+    return request(GET, url);
 }
 
 export function fetchWitnessAnnotations(witness) {
     const url = '/api/texts/'+witness.text+'/witnesses/'+witness.id+'/annotations/';
-    return getData(url);
+    return request(GET, url);
 }
 
-function getApplyAnnotationUrl(witness) {
-    console.log('getApplyAnnotationUrl: %o', witness);
-    return '/api/texts/'+witness.text.id+'/witnesses/'+witness.id+'/applied_annotations/';
+// APPLYING ANNOTATIONS
+
+function getApplyAnnotationUrl(witness, annotationId=null) {
+    let url = '/api/texts/'+witness.text.id+'/witnesses/'+witness.id+'/applied_annotations/';
+    if (annotationId) {
+        url += annotationId;
+    }
+    return url;
 }
 
 export function fetchAppliedUserAnnotations(witness) {
     const url = getApplyAnnotationUrl(witness);
-    return getData(url);
+    return request(GET, url);
 }
 
 export function applyAnnotation(annotation) {
     const url = getApplyAnnotationUrl(annotation.witness);
 
-    return new Promise((resolve, reject) => {
-        axios.post(url, {
-                annotation_id: annotation.id
-            })
-            .then(function(response){
-                resolve(response.data);
-            })
-            .catch(function (error) {
-                reject(error);
-            })
+    return request(POST, url, {
+        annotation_id: annotation.id
     });
 }
 
-export function removeAnnotation(annotation) {
-    const url = getApplyAnnotationUrl(annotation.witness);
+export function removeAppliedAnnotation(annotation) {
+    const url = getApplyAnnotationUrl(annotation.witness, annotation.id);
+    return request(DELETE, url);
+}
 
-    return new Promise((resolve, reject) => {
-        axios.delete(url, {
-                annotation_id: annotation.id
-            })
-            .then(function(response){
-                resolve(response.data);
-            })
-            .catch(function (error) {
-                reject(error);
-            })
-    });
 }
