5fca6d00b39df5c8546da2cf666429277abe9962
Tim Lewis <tim@phurba.net>
2017-09-17 19:24:39 +1200

    Order the tabs with the working version first, then the base, then others.

--- parkhang/frontend/app/components/TextDetail/TabBar.js
+++ parkhang/frontend/app/components/TextDetail/TabBar.js
@@ -10,7 +10,23 @@ export default class TabBar extends React.Component {
     render() {
         let tabs = [];
         if (this.props.witnesses) {
-            for (let witness of this.props.witnesses) {
+            const witnesses = this.props.witnesses.map(witness => witness);
+            witnesses.sort((a, b) => {
+                if (a.isWorking) {
+                    return -1;
+                }
+                if (b.isWorking) {
+                    return 1;
+                }
+                if (a.isBase) {
+                    return -1;
+                }
+                if (b.isBase) {
+                    return 1;
+                }
+                return 0;
+            });
+            for (let witness of witnesses) {
                 let classes = [styles.tab];
                 if (witness === this.props.activeWitness) {
                     classes.push(styles.selected);
