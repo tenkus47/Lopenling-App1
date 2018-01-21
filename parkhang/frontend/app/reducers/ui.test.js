d9058d61eeb52a6a79fc667c8ccc7b4fe7932a37
Tim Lewis <tim@phurba.net>
2018-01-22 02:16:02 +1300

    Update tests to use new ui state shape.

--- parkhang/frontend/app/reducers/ui.test.js
+++ parkhang/frontend/app/reducers/ui.test.js
@@ -10,12 +10,22 @@ const source1 = new Source(1, "Derge");
 const text = new Text(1, "དཔལ་ནག་པོ་ཆེན་པོ་ཡུམ་ཅན་གྱི་སྒྲུབ་ཐབས་ཞེས་བྱ་བ");
 const baseWitness = new Witness(1, text, source1, "Test witness", true);
 const user = new User(1, 'Test User');
-const state = initialUIState;
+let state = initialUIState;
+
+state = {
+    ...state,
+    selectedText: {
+        id: 1
+    },
+    selectedTextWitness: {
+        1: 1
+    }
+};
 
 
 test('addedTemporaryAnnotation', () => {
 
-    const annotation = new TemporaryAnnotation(null, baseWitness, 0, 27, "༄༅༅", ANNOTATION_TYPES.variant, null, user);
+    const annotation = new TemporaryAnnotation(null, baseWitness, 0, 27, "༄༅༅", ANNOTATION_TYPES.variant, baseWitness, user);
     const action = actions.addedTemporaryAnnotation(annotation, true);
 
     const expectedState = {
@@ -40,8 +50,8 @@ test('addedTemporaryAnnotation', () => {
 
 test('removedTemporaryAnnotation', () => {
 
-    const annotation = new TemporaryAnnotation(null, baseWitness, 0, 27, "༄༅༅", ANNOTATION_TYPES.variant, null, user);
-    const extraAnnotation = new TemporaryAnnotation(null, baseWitness, 28, 1, "ཀ", ANNOTATION_TYPES.variant, null, user);
+    const annotation = new TemporaryAnnotation(null, baseWitness, 0, 27, "༄༅༅", ANNOTATION_TYPES.variant, baseWitness, user);
+    const extraAnnotation = new TemporaryAnnotation(null, baseWitness, 28, 1, "ཀ", ANNOTATION_TYPES.variant, baseWitness, user);
     const addAction = actions.addedTemporaryAnnotation(annotation, true);
     let testState = uiReducers[addAction.type](state, addAction);
     const addAction2 = actions.addedTemporaryAnnotation(extraAnnotation, true);
