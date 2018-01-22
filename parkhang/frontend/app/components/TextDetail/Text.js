0cac7a323a06a23f72f803949c3e46dceeacb027
Tim Lewis <tim@phurba.net>
2018-01-22 19:48:25 +1300

    Change render optimisations to be more reliable.

--- parkhang/frontend/app/components/TextDetail/Text.js
+++ parkhang/frontend/app/components/TextDetail/Text.js
@@ -70,102 +70,110 @@ export default class Text extends React.Component {
         this.props.selectedSegmentId(element.id);
     }
 
-    generateHtml() {
-        let segments = this.state.segmentedText.sortedSegments();
-        let activeWitness = this.props.activeWitness;
-        if (segments !== this._renderedSegments || this._renderedHtml === null) {
-            let segmentHTML = '';
-            const insertionClass = styles.insertion;
-            const endPosition = segments[segments.length - 1].end + 1;
-            if (this.props.annotationPositions[INSERTION_KEY + endPosition]) {
-                const endSegment = new TextSegment(endPosition, "");
-                segments.push(endSegment);
-            }
-            for (let i = 0; i < segments.length; i++) {
-                let segment = segments[i];
-                let classAttribute = "";
-                let classes = [];
-                let annotations = this.annotationsForSegment(segment);
-                let deletionText = null;
-                let selectedCurrentDeletion = false;
-                if (annotations) {
-                    let insertions = annotations.filter((annotation) => annotation.isInsertion);
-                    let activeInsertions = _.intersectionWith(
-                        this.props.activeAnnotations,
-                        insertions,
-                        (a, b) => a.uniqueId === b.uniqueId);
-                    let inactiveInsertions = _.differenceWith(insertions, activeInsertions, (a, b) => a.start === b.start);
-                    if (inactiveInsertions.length > 0) {
-                        const insertion = inactiveInsertions[0];
-                        const insertionId = idForInsertion(segment);
-
-                        segmentHTML += '<span id=' + insertionId + ' key=' + insertionId + ' class="' + insertionClass + '">'+insertion.content+'</span>';
-                    }
-
-                    let remainingAnnotations = _.differenceWith(annotations, insertions, (a, b) => a.id == b.id);
-
-                    let deletions = remainingAnnotations.filter((annotation) => annotation.isDeletion);
-                    let activeDeletions = _.intersectionWith(
-                        this.props.activeAnnotations,
-                        deletions,
-                        (a, b) => a.id === b.id
-                    );
-                    if (activeDeletions.length > 0) {
-                        // assume any other deletions are the same
-                        remainingAnnotations = remainingAnnotations.filter((annotation) => !annotation.isDeletion);
-                        const activeDeletion = activeDeletions[0];
-                        const baseAnnotation = this.props.getBaseAnnotation(activeDeletion);
-                        deletionText = baseAnnotation.content;
-                        if (
-                            this.props.activeAnnotation
-                            && this.props.activeAnnotation.isDeletion
-                            && this.props.activeAnnotation.start == activeDeletion.start
-                            && this.props.activeAnnotation.length == activeDeletion.length
-                            && segment.length === 0
-                        ) {
-                            selectedCurrentDeletion = true;
-                        }
-                    }
-
-                    if (remainingAnnotations.length > 0 || activeInsertions.length > 0) {
-                        classes.push(styles.annotation);
-                    }
-                }
-                // It's an insertion at the end of the text, which should have just been added to the html.
-                // So break as we don't want anymore segment html adding.
-                if (segment.start === endPosition) {
-                    break;
-                }
-                let id = null;
-                if (segment.length == 0) {
-                    id = idForDeletedSegment(segment);
-                    classes.push(styles.removedByAnnotation);
-                    if (deletionText) {
-                        segment = new TextSegment(segment.start, deletionText);
-                    }
-                } else {
-                    id = idForSegment(segment);
-                }
-
-                if (this.segmentsContainSegment(this.props.selectedAnnotatedSegments, segment) || selectedCurrentDeletion) {
-                    classes.push(styles.selectedAnnotation);
-                }
-
-                if (classes.length > 0) {
-                    let className = classnames(...classes);
-                    classAttribute = 'class="' + className + '"';
-                }
-
-                segmentHTML += '<span id=' + id + ' key=' + id + ' ' + classAttribute + '>' + segment.text + '</span>';
-            }
-            this._renderedSegments = segments;
-            this._renderedHtml = segmentHTML;
+    generateHtml(renderProps, renderState) {
+        let segments = renderState.segmentedText.sortedSegments();
+        let segmentHTML = '';
+        const insertionClass = styles.insertion;
+        const endPosition = segments[segments.length - 1].end + 1;
+        if (renderProps.annotationPositions[INSERTION_KEY + endPosition]) {
+            const endSegment = new TextSegment(endPosition, "");
+            segments.push(endSegment);
         }
+        for (let i = 0; i < segments.length; i++) {
+            let segment = segments[i];
+            let classAttribute = "";
+            let classes = [];
+            let annotations = this.annotationsForSegment(segment);
+            let deletionText = null;
+            let selectedCurrentDeletion = false;
+            if (annotations) {
+                let insertions = annotations.filter((annotation) => annotation.isInsertion);
+                let activeInsertions = _.intersectionWith(
+                    renderProps.activeAnnotations,
+                    insertions,
+                    (a, b) => a.uniqueId === b.uniqueId);
+                let inactiveInsertions = _.differenceWith(insertions, activeInsertions, (a, b) => a.start === b.start);
+                if (inactiveInsertions.length > 0) {
+                    const insertion = inactiveInsertions[0];
+                    const insertionId = idForInsertion(segment);
+
+                    segmentHTML += '<span id=' + insertionId + ' key=' + insertionId + ' class="' + insertionClass + '">'+insertion.content+'</span>';
+                }
+
+                let remainingAnnotations = _.differenceWith(annotations, insertions, (a, b) => a.id == b.id);
+
+                let deletions = remainingAnnotations.filter((annotation) => annotation.isDeletion);
+                let activeDeletions = _.intersectionWith(
+                    renderProps.activeAnnotations,
+                    deletions,
+                    (a, b) => a.id === b.id
+                );
+                if (activeDeletions.length > 0) {
+                    // assume any other deletions are the same
+                    remainingAnnotations = remainingAnnotations.filter((annotation) => !annotation.isDeletion);
+                    const activeDeletion = activeDeletions[0];
+                    const baseAnnotation = renderProps.getBaseAnnotation(activeDeletion);
+                    deletionText = baseAnnotation.content;
+                    if (
+                        renderProps.activeAnnotation
+                        && renderProps.activeAnnotation.isDeletion
+                        && renderProps.activeAnnotation.start == activeDeletion.start
+                        && renderProps.activeAnnotation.length == activeDeletion.length
+                        && segment.length === 0
+                    ) {
+                        selectedCurrentDeletion = true;
+                    }
+                }
+
+                if (remainingAnnotations.length > 0 || activeInsertions.length > 0) {
+                    classes.push(styles.annotation);
+                }
+            }
+            // It's an insertion at the end of the text, which should have just been added to the html.
+            // So break as we don't want anymore segment html adding.
+            if (segment.start === endPosition) {
+                break;
+            }
+            let id = null;
+            if (segment.length == 0) {
+                id = idForDeletedSegment(segment);
+                classes.push(styles.removedByAnnotation);
+                if (deletionText) {
+                    segment = new TextSegment(segment.start, deletionText);
+                }
+            } else {
+                id = idForSegment(segment);
+            }
+
+            if (this.segmentsContainSegment(renderProps.selectedAnnotatedSegments, segment) || selectedCurrentDeletion) {
+                classes.push(styles.selectedAnnotation);
+            }
+
+            if (classes.length > 0) {
+                let className = classnames(...classes);
+                classAttribute = 'class="' + className + '"';
+            }
+
+            segmentHTML += '<span id=' + id + ' key=' + id + ' ' + classAttribute + '>' + segment.text + '</span>';
+        }
+
+        this._renderedSegments = segments;
+
         const html = {
-            __html: this._renderedHtml
+            __html: segmentHTML
         };
 
         return html;
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        const renderedHtml = this.generateHtml(nextProps, nextState);
+        if (renderedHtml.__html == this._renderedHtml.__html) {
+            return false;
+        } else {
+            this._renderedHtml = renderedHtml;
+            return true;
+        }
     }
 
     render() {
@@ -179,7 +187,10 @@ export default class Text extends React.Component {
 
         // Generate HTML manually as it is much faster when
         // creating large numbers of elements, such as these spans.
-        const html = this.generateHtml();
+        const html = (this._renderedHtml) ? this._renderedHtml : this.generateHtml(this.props, this.state);
+        if (!this._renderedHtml) {
+            this._renderedHtml = html;
+        }
         const containerStyle = {
             paddingRight: this.props.paddingRight
         };
