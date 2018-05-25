487c50c29b9faca5d2c32899cdcd662ba069ee84
Tim Lewis <tim@phurba.net>
2018-05-25 23:41:35 +1200

    Fix test to create annotatedTexts correctly.

--- parkhang/frontend/app/lib/SplitText.test.js
+++ parkhang/frontend/app/lib/SplitText.test.js
@@ -14,14 +14,14 @@ function segmenter(text) {
 
 const baseTextContent =
     "༄༅། །སྒྲུབ་ཐབས་ཞེས་བྱ་བ།༄༅༅། །རྒྱ་གར་སྐད་དུ།། སྟྲཱི་པྲཛྙཱ་ཤྲཱི་མ་ཧཱ་ཀཱ་ལ་སཱ་དྷ་ནཾ་ནཱ་མ། བོད་སྐད་དུ། དཔལ་ནག་པོ་ཆེན་པོ་ཡུམ་ཅན་གྱི་སྒྲུབ་ཐབས་ཞེས་བྱ་བ།། བླ་མ་དང་དཔལ་རྡོ་རྗེ་མཁའ་འགྲོ་ལ་ཕྱག་འཚལ་ལོ། །འགྱེལ་བའི་རོ་ལ་ཞབས་མཆོག་མཉམ་པའི་སྟབས་ཀྱིས་བཞུགས་ཤིང་སྦོམ་ཐུང་དྲག་ལ་གསུས་ཁྱིམ་ཡངས།";
-const segmentedText = segmentTibetanText(baseTextContent);
-const annotatedText = new AnnotatedText(segmentedText, [], segmenter);
-
 const source1 = new Source(1, "Derge");
 const source2 = new Source(2, "Narthang");
 const text = new Text(1, "དཔལ་ནག་པོ་ཆེན་པོ་ཡུམ་ཅན་གྱི་སྒྲུབ་ཐབས་ཞེས་བྱ་བ");
 const baseWitness = new Witness(1, text, source1, baseTextContent, true);
 const otherWitness = new Witness(2, text, source2);
+
+const segmentedText = segmentTibetanText(baseTextContent);
+const annotatedText = new AnnotatedText(segmentedText, [], segmenter, baseWitness);
 
 const splitter = stringSplitter("།།");
 const splitText = new SplitText(annotatedText, splitter);
