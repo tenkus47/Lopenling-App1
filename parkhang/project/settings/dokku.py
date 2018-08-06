aa7131273aeb600a41bd36794b66230798f35621
Tim Lewis <tim@phurba.net>
2018-08-06 23:51:36 +1200

    Add generated bundle files to static files dirs.

--- parkhang/project/settings/dokku.py
+++ parkhang/project/settings/dokku.py
@@ -1,10 +1,16 @@
 from .base import *
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 PROD_ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ALLOWED_HOSTS + PROD_ALLOWED_HOSTS
+
+STATICFILES_DIRS = STATICFILES_DIRS + [
+    '%s/../frontend/static/' % (BASE_DIR),
+]
 
 STATIC_ROOT = '/app/storage/static_collected/'
 MEDIA_ROOT = '/app/storage/media'
