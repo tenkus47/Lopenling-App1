6e00b0c7a25fdeb699e152b518234156513fbb1c
Tim Lewis <tim@phurba.net>
2019-01-08 23:07:25 +1300

    Remove test for initialState.
    It has proven to be very brittle while providing little value.

--- parkhang/frontend/app/reducers/reducers.test.js
+++ parkhang/frontend/app/reducers/reducers.test.js
@@ -1,54 +1,9 @@
 // @flow
 import rootReducer, { dataReducer } from "reducers";
 import * as actions from "actions";
-import User, { getAnonymousUser } from "lib/User";
-
-const anonymousUser = getAnonymousUser();
-
-const initialState = {
-    data: {
-        texts: [],
-        textsById: {},
-        sources: [],
-        sourcesById: {},
-        textWitnessesById: {},
-        witnessesById: {},
-        witnessAnnotationsById: {},
-        witnessActiveAnnotationsById: {},
-        loadingInitialData: false,
-        loadedInitialData: false,
-        loadingTexts: false,
-        loadedTexts: false,
-        loadingSources: false,
-        loadedSources: false,
-        loadingWitnesses: false,
-        loadedWitnesses: false,
-        loadingAnnotations: false,
-        loadedAnnotations: false,
-        loadedAppliedAnnotations: false
-    },
-    ui: {
-        selectedText: null,
-        selectedTextWitness: {},
-        searchValue: "",
-        showPageImages: false,
-        activeAnnotations: {},
-        textListVisible: true,
-        temporaryAnnotations: {}
-    },
-    user: {
-        userId: anonymousUser.id,
-        userName: anonymousUser.name
-    }
-};
-
-describe("Root reducer", () => {
-    it("should return initial state", () => {
-        expect(rootReducer(undefined, {})).toEqual(initialState);
-    });
-});
 
 describe("Texts reducer", () => {
+    let initialState = rootReducer(undefined, {});
     it("should handle loadedInitialData", () => {
         expect(
             dataReducer(initialState.data, actions.loadedInitialData())
