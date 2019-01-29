d9f897b608226c97051300ab521c7b79167da367
Tim Lewis <tim@phurba.net>
2019-01-29 22:10:10 +1300

    Switch docx import to dynamic.

--- parkhang/frontend/app/lib/DocxExporter.js
+++ parkhang/frontend/app/lib/DocxExporter.js
@@ -1,5 +1,4 @@
 // @flow
-import { Document, Paragraph, Packer } from "docx";
 import type { TextExporter } from "./TextExporter";
 import AnnotatedText from "./AnnotatedText";
 import lengthSplitterFactory from "lib/text_splitters/lengthSplitter";
@@ -9,8 +8,13 @@ const HEADING = "TibetanHeading";
 const TEXT = "TibetanText";
 
 class DocxExporter implements TextExporter {
-    export(text: AnnotatedText, filename: string): Promise<Blob> {
-        const doc = new Document();
+    async export(text: AnnotatedText, filename: string): Promise<Blob> {
+        // We are importing dynamically as docx is a large library that
+        // is only required occasionally, thus not worth slowing down
+        // the initial load for.
+        const docx = await import(/* webpackChunkName: "docx" */ "docx");
+
+        const doc = new docx.Document();
 
         this.generateStyles(doc);
 
@@ -24,21 +28,20 @@ class DocxExporter implements TextExporter {
         while (positions.length > 0) {
             const pos = positions.shift();
             const paraContent = content.substring(startPos, pos);
-            const para = new Paragraph(paraContent).style(TEXT);
+            const para = new docx.Paragraph(paraContent).style(TEXT);
             doc.addParagraph(para);
             startPos = pos;
         }
 
-        const packer = new Packer();
-
-        return new Promise(resolve => {
-            packer.toBlob(doc).then(blob => {
-                resolve(blob);
-            });
-        });
+        const packer = new docx.Packer();
+        const blob = await packer.toBlob(doc);
+        return blob;
     }
 
     generateStyles(doc: Document) {
+        // As we are now dynamically importing docx, these types are not
+        // available until runtime.
+        //$FlowFixMe
         doc.Styles.createParagraphStyle(HEADING, HEADING)
             .basedOn("Normal")
             .size(28)
@@ -46,7 +49,7 @@ class DocxExporter implements TextExporter {
             .spacing({
                 after: 120
             });
-
+        //$FlowFixMe
         doc.Styles.createParagraphStyle(TEXT, TEXT)
             .basedOn("Normal")
             .size(18)
