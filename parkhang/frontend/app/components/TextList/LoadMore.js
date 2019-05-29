784d2b35559496d7eeb5e65550443c22d3baaf8f
Tim Lewis <tim@phurba.net>
2019-05-29 23:39:04 +1200

    Localise LoadMore.

--- parkhang/frontend/app/components/TextList/LoadMore.js
+++ parkhang/frontend/app/components/TextList/LoadMore.js
@@ -1,5 +1,6 @@
 // @flow
 import React from "react";
+import { FormattedMessage, injectIntl } from "react-intl";
 import styles from "./LoadMore.css";
 
 type Props = {
@@ -9,9 +10,17 @@ type Props = {
 export default function LoadMore(props: Props) {
     let content;
     if (props.loading) {
-        content = <span>Loading…</span>;
+        content = (
+            <span>
+                <FormattedMessage id="leftbar.loading" />
+            </span>
+        );
     } else {
-        content = <span>Load More…</span>;
+        content = (
+            <span>
+                <FormattedMessage id="leftbar.loadMore" />
+            </span>
+        );
     }
     return <div className={styles.container}>{content}</div>;
 }
