c9cbda1e33d42a612898fc75adb9dfc48dce8de1
Tim Lewis <tim@phurba.net>
2019-09-13 23:36:58 +1200

    Add email variables to dokku settings

--- parkhang/project/settings/dokku.py
+++ parkhang/project/settings/dokku.py
@@ -8,6 +8,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ALLOWED_HOSTS + PROD_ALLOWED_HOSTS
 
+# EMAIL Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
 STATICFILES_DIRS = STATICFILES_DIRS + [
     '%s/../frontend/static/' % (BASE_DIR),
 ]
