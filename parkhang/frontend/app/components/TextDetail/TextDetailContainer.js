2ff8e28a871b4ed8cfbdff6a1d173b5a2c9979e8
Tim Lewis <tim@phurba.net>
2019-11-07 23:57:52 +1300

    Add check for annotatedText version when retrieving positions from cache.

--- parkhang/frontend/app/components/TextDetail/TextDetailContainer.js
+++ parkhang/frontend/app/components/TextDetail/TextDetailContainer.js
@@ -132,6 +132,7 @@ const getAnnotationPositions = (
     _posAnnotatedText = annotatedText;
     _posAnnotations = annotations;
     _positions = positions;
+    _posVersion = annotatedText.version;
     return positions;
 };
 
