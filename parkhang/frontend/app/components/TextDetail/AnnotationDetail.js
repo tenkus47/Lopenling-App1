4b0fc4ec4245c1c8e133772d46386c473db44fb0
tenkus47 <tenkus47@gmail.com>
2022-04-19 19:05:25 +0530

    Share Button Relocation Complete

--- parkhang/frontend/app/components/TextDetail/AnnotationDetail.js
+++ parkhang/frontend/app/components/TextDetail/AnnotationDetail.js
@@ -6,7 +6,7 @@ import styles from "./AnnotationDetail.css";
 import type { AnnotationData } from "api";
 import CheckIcon from "images/check_circle.svg";
 import colours from "css/colour.css";
-
+import ShareIcon from 'images/share.svg';
 import {FacebookShareButton,
     WhatsappShareButton,
     FacebookIcon,
@@ -65,48 +65,6 @@ const AnnotationDetail = (props: Props) => {
        
      }
 
-    const ImageDownload=()=>{
-        var fontSize= props.fontSize;
-        var text = props.annotationData.content
-        var lineheight = 50;
-        var ra = text.split(' ')
-        var lines = mergeArray(ra)
-        var canvas = document.createElement("canvas");
-        canvas.height = lines.length * lineheight+30;
-        canvas.width =90 + 6 * longest_str_in_array(lines);
-        
-        var ctx = canvas.getContext('2d');
-       
-            var grad = ctx.createRadialGradient(300, 100, 0, 300, 100, 316.23);
-            grad.addColorStop(0, 'rgba(255, 255, 0, 1)');
-            grad.addColorStop(0.57, 'rgba(0, 188, 212, 1)');
-            grad.addColorStop(1, 'rgba(238, 130, 238, 1)');
-            ctx.fillStyle=grad;
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-       
-      
-        ctx.font = `${fontSize}`+"px Arial";
-        ctx.fillStyle="black"
-        
-        for (var i = 0; i<lines.length; i++){
-                       ctx.fillText(lines[i].concat(' '), 35, 35 + (i*lineheight) );
-                        }
-        var data = canvas.toDataURL();
-        var img = document.createElement('img');
-        img.src = data;
-        setImageUrl(data);
-        // runClick(data);
-        // creating a clickable link for downloading image
-        var a = document.createElement('a');
-        a.setAttribute("download", "SelectedText.jpeg");
-        a.setAttribute("href", data);
-        a.appendChild(img);
-        
-        var w = open();
-        w.document.title = props.annotationData.content.slice(0,30)+'...';
-        w.document.body.innerHTML = '<h1>click on Image to Download</h1>';
-        w.document.body.appendChild(a);
-       }
 
     let desc = (
          <p>
@@ -127,22 +85,6 @@ const AnnotationDetail = (props: Props) => {
 
     let classes = [styles.annotationDetail];
   
-const runClick=()=>{
-    var imgURL = imageUrl
-    FB.api('/photos', 'post', {
-        message: 'photo description',
-        access_token:"EAAW41ADJQ7kBAF73Bls1ZAqmeVZCuZBgYZAfPPXdVPxCvikV5OCZA229RbB1USuPLUlwVmkqihZAO5NZCRua0080zKmZCvAZAS5Kw6vw8noAUahNYdnTDYcbl062BBBADkhptyjpnxaMxzu1ehxMR1B32Sbn6IAZBmZAWzms3KPy0ahGy2Bgvev0zY1qcsn5Y4ZB9xIBqD5R6cZBLqrZB5zdBt1hoKsXn585wTLacoqVxMHMHZCSrVHepDZBZBBZCG",
-        url: imgURL
-    }, function (response) {
-
-        if (!response || response.error) {
-            alert('Error occured:' + response);
-        } else {
-            alert('Post ID: ' + response.id);
-        }
-
-    });
-}
 
 
     if (props.isActive) {
@@ -192,8 +134,11 @@ const runClick=()=>{
             </div>
 
             {desc}
-           
-                   <Sharebutton props={props}/>
+            
+
+
+
+            <Sharebutton props={props}/>
                       
         </div>
     );
@@ -206,7 +151,10 @@ const runClick=()=>{
 const Sharebutton=({props})=>{
     return (
         <div className={styles.shareContainer}>
-            <div className={styles.shareButton}> <FormattedMessage id="annotation.share" /></div>
+            <div className={styles.shareButton}>
+                 <FormattedMessage id="annotation.share" />
+              
+                 </div>
             <FacebookShareButton
             className={styles.facebookButton}
                 id="sharebutton"
