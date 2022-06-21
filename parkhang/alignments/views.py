4b9a516d3a183eb92458681a1b23fae414e7953a
tenzin <ten13zin@gmail.com>
2022-06-21 23:03:39 +0530

    add(alignment): text, image, video alignment detail api view

--- parkhang/alignments/views.py
+++ parkhang/alignments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import ImageAlignment, TextAlignment, VideoAlignment
+from .serializers import (ImageAlignmentSerializer, TextAlignmentSerializer,
+                          VideoAlignmentSerializer)
+
+
+class DetailTextAlignment(generics.RetrieveAPIView):
+    queryset = TextAlignment.objects.all()
+    serializer_class = TextAlignmentSerializer
+
+class DetailImageAlignment(generics.RetrieveAPIView):
+    queryset = ImageAlignment.objects.all()
+    serializer_class = ImageAlignmentSerializer
+
+class DetailVideoAlignment(generics.RetrieveAPIView):
+    queryset = VideoAlignment.objects.all()
+    serializer_class = VideoAlignmentSerializer
+
