487f59745980600889d1508c2d2e87726a45f591
tenzin <ten13zin@gmail.com>
2022-06-24 12:50:15 +0530

    feat(texts): add models to admin

--- parkhang/texts/admin.py
+++ parkhang/texts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Source, Text, Topic
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_base", "is_working")
+
+@admin.register(Text)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "code", "author")
