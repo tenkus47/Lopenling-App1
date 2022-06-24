7c8c7bc80bd75db8fdf5416f3d579016a3f5e90f
tenzin <ten13zin@gmail.com>
2022-06-24 13:05:09 +0530

    feat(texts): add Witness and Question model to admin

--- parkhang/texts/admin.py
+++ parkhang/texts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Author, Source, Text, Topic
+from .models import (Annotation, Author, DefaultWitnessAnnotations, Question,
+                     Source, Text, Topic, UserAnnotationOperation, Witness)
 
 
 @admin.register(Author)
@@ -18,3 +19,11 @@ class SourceAdmin(admin.ModelAdmin):
 @admin.register(Text)
 class TextAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "code", "author")
+
+@admin.register(Witness)
+class WitnessAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "source", "revision")
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "annotation", "topic_id", "title", "created")
