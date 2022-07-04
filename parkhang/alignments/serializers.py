f0379350cd4996584edae5c8b9e0a09c8278ed39
10zinten <ten13zin@gmail.com>
2022-07-04 20:35:47 +0530

    fix(alignments): add text id to soruce and targret

--- parkhang/alignments/serializers.py
+++ parkhang/alignments/serializers.py
@@ -5,6 +5,9 @@ from .models import ImageAlignment, TextAlignment, Video, VideoAlignment
 
 class TextAlignmentSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+    source = serializers.SerializerMethodField()
+    target = serializers.SerializerMethodField()
+
     class Meta:
         model = TextAlignment
         fields = ("id", "source", "target", "type", "alignment")
@@ -12,9 +15,18 @@ class TextAlignmentSerializer(serializers.ModelSerializer):
     def get_type(self, obj):
         return obj.get_type_display()
 
+    def get_source(self, witness):
+        text_id = witness.text.id
+        return f"{text_id}/{witness.id}"
+
+    def get_target(self, witness):
+        text_id = witness.text.id
+        return f"{text_id}/{witness.id}"
+
 
 class ImageAlignmentSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+
     class Meta:
         model = ImageAlignment
         fields = ("id", "source", "target", "type", "alignment")
@@ -22,14 +34,17 @@ class ImageAlignmentSerializer(serializers.ModelSerializer):
     def get_type(self, obj):
         return obj.get_type_display()
 
+
 class VideoAlignmentSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+
     class Meta:
         model = VideoAlignment
         fields = ("id", "source", "target", "type", "alignment")
 
     def get_type(self, obj):
         return obj.get_type_display()
+
 
 class TargetVideoSerializer(serializers.ModelSerializer):
     class Meta:
